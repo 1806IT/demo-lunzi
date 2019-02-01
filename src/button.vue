9ea2379f8aed8d086fd07c4c8174c26f5dba4b0f
zzc <370531378@qq.com>
2019-02-02 04:44:23 +0800

    添加icon和iconPosition

--- src/button.vue
+++ src/button.vue
@@ -1,30 +1,61 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <!--<button class="g-button" :class="{'undefined':true}">-->
+        <!--<button class="g-button" :class="{'left':true}">-->
+        <!--<button class="g-button" :class="{'right':true}">-->
+        <svg v-if="icon" class="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
     export default {
-        // name:'g-button'
-
+        name: 'g-button',
+        props: ['icon', 'iconPosition']
     }
 </script>
 
 <style lang="scss">
-    .g-button{
-        font-size:var(--font-size);
+    .g-button {
+        font-size: var(--font-size);
         height: var(--button-height);
         background: var(--button-bg);
-        border:1px solid var(--border-color);
+        border: 1px solid var(--border-color);
         border-radius: var(--border-radius);
         padding: 0 1em;
-        &:hover{
+        display:inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: top;
+        &:hover {
             border-color: var(--border-color-hover);
         }
-        &:active{
-            background:var(--button-active-bg)
+        &:active {
+            background: var(--button-active-bg)
         }
-        &:focus{
+        &:focus {
             outline: none;
+        }
+        > .icon{
+            order: 1;
+            margin-right: .3em;
+        }
+        > .content{
+            order:2;
+        }
+        &.icon-right{
+            > .icon{
+                order:2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
+            > .content{
+                order:1;
+            }
         }
     }
 
