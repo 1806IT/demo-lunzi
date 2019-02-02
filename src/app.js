e9f98f0b1c5b5fdbfb695760ed5aec3e77c5ad6a
zzc <370531378@qq.com>
2019-02-02 13:11:16 +0800

    使用chair expect创建测试用例，第五个事件的测试用例不会

--- src/app.js
+++ src/app.js
@@ -14,3 +14,87 @@ new Vue({
         loading3:false
     }
 })
+
+import chai from 'chai'
+
+const expect=chai.expect
+// 单元测试
+//测试button组件输入参数icon
+{
+    // console.log(Button);
+    const Constructor=Vue.extend(Button)
+    const button=new Constructor({
+        propsData:{
+            icon:'setting'
+        }
+    })
+    button.$mount('#test')
+    // 动态生成按钮
+    let useElement=button.$el.querySelector('use')
+    console.log(useElement);
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-setting')
+}
+//测试button组件输入参数loading
+{
+    const Constructor=Vue.extend(Button)
+    const button=new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true
+        }
+    })
+    button.$mount()
+    // 动态生成按钮
+    let useElement=button.$el.querySelector('use')
+    let href=useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+}
+// 测试button组件输入参数iconPosition
+{
+    const div=document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor=Vue.extend(Button)
+    const button=new Constructor({
+        propsData:{
+            icon:'loading',
+            iconPosition:'right'
+        }
+    })
+    button.$mount(div)
+    let svg=button.$el.querySelector('svg')
+    let {order}=window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    button.$el.remove()
+    // button.$destorye()
+}
+{
+    const div=document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor=Vue.extend(Button)
+    const vm=new Constructor({
+        propsData:{
+            icon:'loading',
+        }
+    })
+    vm.$mount(div)
+    let svg=vm.$el.querySelector('svg')
+    let {order}=window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+}
+//测试button的点击事件
+//mock
+{
+    const Constructor=Vue.extend(Button)
+    const gButton=new Constructor({
+        propsData:{
+            icon:'loading',
+        }
+    })
+    gButton.$mount()
+    gButton.$on('click',function(){
+        console.log(1)
+    })
+    let button=gButton.$el
+    button.click()
+
+}
