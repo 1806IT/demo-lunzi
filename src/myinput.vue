67a2680ae5f2a7a68c52aec94b0e818ef3d0f450
zzc <370531378@qq.com>
2019-02-04 05:14:26 +0800

    添加input.vue及其样式

---
+++ src/myinput.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="wrapper" :class="{error}">
+        <input type="text" :value="value" :disabled="disabled" :readonly="readonly"
+            @change="$emit('change')"
+        >
+        <template v-if="error">
+            <icon name="setting"></icon>
+            <span>{{error}}</span>
+        </template>
+
+    </div>
+</template>
+
+<script>
+    import Icon from './icon'
+
+    export default {
+        name: "lunzi-input",
+        props: {
+            value: {
+                type: String,
+                default: '请输入'
+            },
+            disabled:{
+                type:Boolean,
+                default: false
+            },
+            readonly:{
+                type:Boolean,
+                default:false
+            },
+            error:{
+                type:String
+            }
+        },
+        components:{
+            Icon
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    $height: 32px;
+    $border-color: #999;
+    $border-color-hover: #666;
+    $font-size: 12px;
+    $box-shadow-color:rgba(0,0,0,0.5);
+    $red:#f1453d;
+    .wrapper {
+        font-size:$font-size;
+        display: inline-flex;
+        align-items: center;
+        >*{
+            margin-right: 0.5em;
+            &:last-child{margin-right: 0}
+        }
+        &.error{
+            >input{
+                border-color: $red;
+            }
+        }
+        > input {
+            border: 1px solid $border-color;
+            height: $height;
+            border-radius: var(--border-radius);
+            padding: 0 8px;
+            &:hover {
+                border-color: $border-color-hover;
+            }
+            &:focus{
+                box-shadow: inset 0 1px 3px $box-shadow-color;
+                outline: none;
+            }
+            &[disabled],&[readonly]{
+                border-color:#ccc;
+                color: #ccc;
+                cursor: not-allowed;
+            }
+            &[readonly]{
+                cursor: not-allowed;
+            }
+        }
+    }
+</style>
