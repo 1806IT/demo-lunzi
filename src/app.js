6389366df1ebe6ae6c105b26be5e80129ea7b592
zzc <370531378@qq.com>
2019-02-05 08:59:12 +0800

    完成row 和col的雏形

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './myinput.vue'
+import Row from './g-row'
+import Col from './g-col'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
 Vue.component('g-input',Input)
+Vue.component('g-row',Row)
+Vue.component('g-col',Col)
 
 new Vue({
     el: "#app",
---
+++ src/g-row.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "grow"
+    }
+</script>
+
+<style scoped lang="scss">
+
+    .row{
+        display: flex;
+        justify-content: center;
+        flex-direction: row;
+    }
+
+
+</style>
