e70e090098316c5ee54a27c08223308e1f1804b1
zzc <370531378@qq.com>
2019-02-10 16:48:24 +0800

    基本实现row和col的布局功能

--- src/g-col.vue
+++ src/g-col.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[span&&`col-${span}`,offset&&`offset-${offset}`]"
+         :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+        <div style="border:1px solid blue;height:50px;">
+            <slot></slot>
+        </div>
+
     </div>
 </template>
 
@@ -8,7 +12,13 @@
     export default {
         name: "gcol",
         props:{
-            span:[Number,String]
+            span:[Number,String],
+            offset:{
+                type:[Number,String]
+            },
+        },
+        data(){
+            return { gutter:null}
         }
     }
 </script>
@@ -19,12 +29,17 @@
     background: grey;
     width: 50%;
     border: 1px solid red;
-
     $class-prefix:col-;
 
     @for $n from 1 through 24{
         &.#{$class-prefix}#{$n}{
             width:($n/24)*100%
+        }
+    }
+    $class-prefix:offset-;
+    @for $n from 1 through 24{
+        &.#{$class-prefix}#{$n}{
+            margin-left:($n/24)*100%
         }
     }
 }
--- src/g-row.vue
+++ src/g-row.vue
@@ -1,12 +1,27 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: "grow"
+        name: "grow",
+        props:{
+            gutter:{
+                type:[String,Number]
+            }
+        },
+        created(){
+            console.log('children:');
+            console.log(this.$children);
+        },
+        mounted(){
+            console.log(this.$children);
+            this.$children.forEach(col=>{
+              col.gutter=this.gutter
+            })
+        }
     }
 </script>
 
@@ -14,8 +29,6 @@
 
     .row{
         display: flex;
-        justify-content: center;
-        flex-direction: row;
     }
 
 
